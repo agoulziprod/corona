f9d4c4a905c3592c70c59c3d469b53191d86e4dd
AGOULZI Mohamed <mohammed.agoulzi50@gmail.com>
2020-03-16 02:56:25 +0100

    data entered

--- js/script.js
+++ js/script.js
@@ -1,19 +1,65 @@
-let images = [{
-    name: "Arab",
-    photos: [{path:"photo.jpg", alt:''},{path:"photo2.jpg", alt:''}]
+let images = [
+    {
+    name: "darija",
+    symbole: "Da",
+    photos: [
+        { path: "darija1.png", alt: '7it kanbghitk, maghansalamch 3lik' },
+        { path: "darija2.png", alt: 'حيت كنبغيك مغنسلمش عليك ' }
+        
+    ]
+},
+    {
+    name: "English",
+    symbole: "En",
+    photos: [
+        { path: "En.png", alt: 'stay distant, stay safe' }
+        
+    ]
+},
+    {
+        name: "Tamazight",
+        symbole: "Ta",
+        photos: [
+            {
+                path: "chelha1.png", alt: 'اديغ شيريغ اورنيغ ادغيفش سلمغ' },
+        { path: "chelha5.png", alt: 'ⵉⴷⵖ ⵖⵉⴼⵉ ⵜⵄⵥⵥⴰⴷ ⵡⵔ ⵏⵏⵉⵖ ⴰⴷⵖⵉⴼⵛ ⵙⵍⵎⵖ' },
+        { path: "chelha3.png", alt: 'Idgh kirigh, Ornigh aghifk sallamagh' },
+            { path: "chelha4.png", alt: 'Mizi tekhseghchek, oukhak tssedjimegh chi' }
+    
+        
+    ]
 },
 ];
+
+
 let contributers = [
     {
-        name: 'smia o knia',
-        Link: 'ima 3amr olla khawi'
+        name: 'AGOULZI Mohamed',
+        Link: 'www.agoulzi.com'
     },
+    {
+        name: 'OTHWSAVE',
+        Link: 'ina link bghiti ?'
+    },
+    {
+        name: 'KHIZOU Hajar',
+        Link: ''
+    },
+    {
+        name: 'AIT OUFKIR Chaimae',
+        Link: ''
+    },
+    {
+        name: 'OUREKOUCH Mounir',
+        Link: ''
+    },
+
 ]
 
 
 
 $('.ui.dropdown')
-  .dropdown()
+    .dropdown()
     ;
 
 $(document).ready(function () {
