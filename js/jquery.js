242e014066ae658be2b489e16a578204686d8bdf
AGOULZI mohamed <mohammed.agoulzi50@gmail.com>
2020-03-16 20:40:55 +0100

    drop down mobile

--- js/jquery.js
+++ js/jquery.js
@@ -2,12 +2,17 @@ $(document).ready(function () {
     $('.ui.dropdown')
         .dropdown()
         ;
+       // $("#lg-mobile").dropdown();
     $(".ui.menu .scrollToImages .menu").on('click', 'a',function (event) {
         event.preventDefault();
         $('html').animate({
             scrollTop: $("#" + $(this).data('scroll')).offset().top - $('header .menu.fixed').innerHeight()
         }, 1200);
     });
+    $(".mobile").on('click',function (event) {
+        alert('test')
+        $('.ui.sidebar').sidebar('toggle')
+    })
     $(".ui.menu .link-to-go").on('click',function (event) {
         event.preventDefault();
         $('html').animate({
@@ -18,6 +23,7 @@ $(document).ready(function () {
     images.forEach(e => {
         $("#source").before(e.template);
         $("#lg").append(`<a class="item" data-scroll="${e.symbole}">${e.name}</a>`)
+        $("#lg-mobile").append(`<a class="item mobile" data-scroll="${e.symbole}">${e.name}</a>`)
     })
     contributers.forEach((e, idx, array)=>{
         $("#contributers-list").append(
