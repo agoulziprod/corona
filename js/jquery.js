a8b610ae063088d476f02ed737e35508f1f7f29c
othwsav <54482114+othwsav@users.noreply.github.com>
2020-03-16 05:07:21 +0100

    gadina za3tar

--- js/jquery.js
+++ js/jquery.js
@@ -8,19 +8,13 @@ $(document).ready(function () {
             scrollTop: $("#" + $(this).data('scroll')).offset().top - $('header .menu.fixed').innerHeight()
         }, 2000);
     });
-    $('.special.cards .image').dimmer({
-        on: 'hover'
-    });
     // appending images object
     images.map(e => {
         $("section.last-sec").before(e.template);
         $("#lg").append(`<a class="item" data-scroll="#${e.symbole}" href="#${e.symbole}">${e.name}</a>`)
     })
-<<<<<<< HEAD
     // contributers hover effect
-  
-||||||| merged common ancestors
-=======
-
->>>>>>> 64aaed0154f80e9891d31212035541e938396bef
+    $('.special.cards .image').dimmer({
+        on: 'hover'
+    });
 });
